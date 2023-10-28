6fa3de6574764454ae96df59faf2b994e29ca04e
Jason Rowland <jsonxr@gmail.com>
2023-10-28 17:01:56 -0500

    test

---
+++ eslint/core.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  extends: ['plugin:promise/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest', 'promise'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'require-await': 'error',
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'warn',
+        'no-undef': 'warn',
+        'no-console': 'off',
+        'jest/no-disabled-tests': 'off',
+        '@typescript-eslint/no-unused-vars': 'warn',
+      },
+    },
+  ],
+};
