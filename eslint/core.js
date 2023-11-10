bed92ba4d6e1656179997f5686ffd14af4f22d92
Jason Rowland <jsonxr@gmail.com>
2023-11-10 11:50:45 -0600

    update with prettier

--- eslint/core.js
+++ eslint/core.js
@@ -1,11 +1,12 @@
 module.exports = {
-  extends: ['plugin:promise/recommended'],
+  extends: ['plugin:promise/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'jest', 'promise'],
+  plugins: ['@typescript-eslint', 'jest', 'promise', 'prettier'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       rules: {
+        'prettier/prettier': 'error',
         '@typescript-eslint/no-unused-vars': 'warn',
         'require-await': 'error',
         'no-shadow': 'warn',
