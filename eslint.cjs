dead54d1b862e78260975562987c81e00e4b977f
Jason Rowland <jsonxr@gmail.com>
2023-11-10 12:33:26 -0600

    chore: update with prefs

--- eslint.cjs
+++ eslint.cjs
@@ -1,19 +1,26 @@
 module.exports = {
-  extends: ['plugin:promise/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'jest', 'promise', 'prettier'],
+  extends: [
+    'plugin:promise/recommended',
+    'prettier',
+    'plugin:jest/recommended',
+  ],
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ['*.ts', '*.tsx', '*.js', '*.jsx', '*.json'],
       rules: {
         'prettier/prettier': 'error',
         '@typescript-eslint/no-unused-vars': 'warn',
         'require-await': 'error',
-        'no-shadow': 'warn',
+        'no-shadow': 'off', // doesn't allow reuse of simple variable names
         'no-undef': 'warn',
-        'no-console': 'off',
-        'jest/no-disabled-tests': 'off'
+        'no-console': 'warn',
+        'jest/no-disabled-tests': 'off', // give me a break, let me disable
       },
     },
   ],
+  env: {
+    jest: true,
+  },
 };
--- prettier.cjs
+++ prettier.cjs
@@ -1,14 +1,14 @@
 module.exports = {
-  "arrowParens": "avoid",
-  "bracketSameLine": true,
-  "bracketSpacing": true,
-  "endOfLine": "lf",
-  "printWidth": 80,
-  "proseWrap": "always",
-  "quoteProps": "consistent",
-  "semi": true,
-  "singleAttributePerLine": false,
-  "singleQuote": true,
-  "tabWidth": 2,
-  "trailingComma": "es5"
-}
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  printWidth: 80,
+  proseWrap: 'always',
+  quoteProps: 'consistent',
+  semi: true,
+  singleAttributePerLine: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+};
